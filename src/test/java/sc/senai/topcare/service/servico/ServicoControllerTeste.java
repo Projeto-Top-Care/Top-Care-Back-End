d50fd31c211d322e83f14c24aea18337b8186b43
Luana Becker <130504315+LuanaBecker24@users.noreply.github.com>
2024-08-27 18:03:45 -0300

    fazendo testes

--- src/test/java/sc/senai/topcare/service/servico/ServicoControllerTeste.java
+++ src/test/java/sc/senai/topcare/service/servico/ServicoControllerTeste.java
@@ -10,15 +10,16 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import sc.senai.topcare.controller.servicos.ServicoController;
 import sc.senai.topcare.entity.*;
 import sc.senai.topcare.repository.ServicoRepository;
 
-import java.util.ArrayList;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
 import java.util.Optional;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -33,8 +34,51 @@ public class ServicoControllerTeste {
     @MockBean
     ServicoRepository servicoRepository;
 
+    private Servico criarServico(){
+        Funcionario funcionario = new Funcionario();
+        funcionario.setId(1L);
+        Periodo periodo = new Periodo(1L, LocalTime.now(), LocalTime.now());
+        Horario horario = new Horario();
+        horario.setId(1L);
+        funcionario.setNome("Funcionario");
+        funcionario.setCelular("232234234224");
+        funcionario.setCpf("223232323");
+        funcionario.setEmail("funcionario@gmail.com");
+        funcionario.setDataNascimento(LocalDate.of(2020, 1, 1));
+        funcionario.setRole(Role.BASIC);
+        funcionario.setSenha("asfsdf");
+        funcionario.setSexo(Sexo.FEMININO);
+        horario.setReservado(false);
+        horario.setDia(LocalDate.now());
+        horario.setHorarios(List.of(periodo));
+        Endereco endereco = new Endereco(1L, "a", "aaa", Estado.SC, "cidade", "bairro", "rua", 123, "aaa");
+        funcionario.setFilial(new Filial(1L, "Nome", endereco));
+        VarianteServico varianteServico = new VarianteServico(1L, "aaa", "aa", 123.0);
+        Especie especie = new Especie();
+        especie.setId(1L);
+        especie.setNome("Especie");
+
+        Servico servico = new Servico(
+                1L,
+                "Nome",
+                "categoria",
+                new File(1L, "Caminho"),
+                "descricao",
+                List.of(funcionario),
+                List.of(especie),
+                List.of(varianteServico)
+        );
+        horario.setServico(servico);
+        funcionario.setServicos(List.of(servico));
+        especie.setServicos(List.of(servico));
+        return servico;
+    }
+
     @Test
-    void testeServicoControllerCadastro() throws Exception {
+    void testeServicoControllerPostOK() throws Exception {
+        when(servicoRepository.save(new Servico()))
+                .then(invocationOnMock -> criarServico());
+
         mockMvc.perform(MockMvcRequestBuilders
                 .post("/servicos")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -52,34 +96,195 @@ public class ServicoControllerTeste {
     }
 
     @Test
-    void testeServicoControllerGetUm() throws Exception {
-        Funcionario funcionario = new Funcionario();
-        Horario horario = new Horario();
-        Endereco endereco = new Endereco();
-        Filial filial = new Filial();
-        Especie especie = new Especie();
+    void testeServicoControllerGetUmOK() throws Exception {
+        when(servicoRepository.findById(1L))
+                .then(invocationOnMock -> Optional.of(criarServico()));
 
         mockMvc.perform(MockMvcRequestBuilders
-                .post("/servicos/1"))
-                .andExpectAll(status()
-                        .isOk(),
+                        .get("/servicos/1"))
+                .andExpect(
+                        status().isOk()
+                ).andExpectAll(
                         jsonPath("$.id").isNumber(),
                         jsonPath("$.imagem").exists(),
                         jsonPath("$.nome").isString(),
-                        jsonPath("$.categoria").exists(),
-                        jsonPath("$.descricao").exists(),
+                        jsonPath("$.categoria").isString(),
+                        jsonPath("$.descricao").isString(),
                         jsonPath("$.funcionarios").isArray(),
                         jsonPath("$.especies").isArray(),
                         jsonPath("$.variantes").isArray(),
+                        jsonPath("$.*").exists()
+                ).andExpectAll(
+                        jsonPath("$.funcionarios").value(Matchers.hasSize(1)),
                         jsonPath("$.funcionarios[0].id").isNumber(),
-                        jsonPath("$.funcionarios[0].nome").isString(),
-                        jsonPath("$.funcionarios[0].*").exists(),
+                        jsonPath("$.funcionarios[0].nome").exists()
+                ).andExpectAll(
                         jsonPath("$.especies[0].id").exists(),
-                        jsonPath("$.especies[0].nome").isString(),
-                        jsonPath("$.especies[0].*").exists(),
-                        jsonPath("$.variantes[0].id").isNumber(),
-                        jsonPath("$.variantes[0].nome").isString(),
-                        jsonPath("$.variantes[0].*").value(Matchers.hasSize(2)));
+                        jsonPath("$.especies[0].nome").exists()
+                ).andExpectAll(
+                        jsonPath("$.variantes[0].tipo").exists(),
+                        jsonPath("$.variantes[0].nome").exists(),
+                        jsonPath("$.variantes[0].preco").isNumber()
+                );
     }
+
+    @Test
+    void testeServicoControllerGetUmNotFound() throws Exception{
+        when(servicoRepository.findById(1L)).then(invocationOnMock ->
+                Optional.empty()
+        );
+        mockMvc.perform(MockMvcRequestBuilders.get("/servicos/1")).andExpect(
+                status().isNotFound()
+        );
+    }
+
+    @Test
+    void testeServicoControllerGetTodos() throws Exception {
+        when(servicoRepository.findAll())
+                .then(invocationOnMock -> List.of(criarServico()));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/servicos")).andExpect(
+                status().isNoContent()
+        ).andExpect(
+                jsonPath("$.*").isArray()
+        ).andExpectAll(
+                jsonPath("$[0].id").isNumber(),
+                jsonPath("$[0].imagem").exists(),
+                jsonPath("$[0].nome").isString(),
+                jsonPath("$[0].categoria").isString(),
+                jsonPath("$[0].descricao").isString(),
+                jsonPath("$[0].funcionarios").isArray(),
+                jsonPath("$[0].especies").isArray(),
+                jsonPath("$[0].variantes").isArray(),
+                jsonPath("$[0].*").exists()
+        ).andExpectAll(
+                jsonPath("$[0].funcionarios").value(Matchers.hasSize(1)),
+                jsonPath("$[0].funcionarios[0].id").isNumber(),
+                jsonPath("$[0].funcionarios[0].nome").exists()
+        ).andExpectAll(
+                jsonPath("$[0].especies[0].id").exists(),
+                jsonPath("$[0].especies[0].nome").exists()
+        ).andExpectAll(
+                jsonPath("$[0].variantes[0].tipo").exists(),
+                jsonPath("$[0].variantes[0].nome").exists(),
+                jsonPath("$[0].variantes[0].*").isNumber()
+        );
+    }
+
+    @Test
+    void testeServicoControllerGetTodosNoContent() throws Exception {
+        when(servicoRepository.findAll())
+                .then(invocationOnMock -> List.of());
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/servicos"))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void testeServicoControllerPutOK() throws Exception{
+        Servico servico = new Servico();
+        when(servicoRepository.save(new Servico())).then(invocationOnMock -> servico);
+        when(servicoRepository.findById(1L)).then(invocationOnMock -> servico);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/servicos/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                    {
+                        "nome": "banho",
+                        "especies": [],
+                        "categoria": "categoria",
+                        "descricao": "descricao",
+                        "funcionarios": [],
+                        "variantes": []
+                    }
+                   """))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testeServicoControllerDeleteNoContent() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.delete("/servicos/1")
+        ).andExpect(status().isNoContent());
+    }
+    @Test
+    void testeServicoControllerDeleteNotFound() throws Exception{
+        doThrow(new NullPointerException())
+                .when(servicoRepository)
+                .deleteById(1L);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/servicos/1")
+        ).andExpect(status().isNotFound());
+    }
+
+    @Test
+    void testeServicoControllerDeleteConflict() throws Exception{
+        doThrow(new SQLIntegrityConstraintViolationException())
+                .when(servicoRepository)
+                .deleteById(1L);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/servicos/1")
+        ).andExpect(status().isConflict());
+    }
+
+    @Test
+    void testeServicoControllerDeleteBadRequest() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.delete("/servicos/1")
+        ).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void testeServicoControllerPostConflict() throws Exception{
+        when(servicoRepository.save(new Servico()))
+                .thenThrow(new SQLIntegrityConstraintViolationException());
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/servicos")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                    {
+                        "nome": "banho",
+                        "especies": [],
+                        "categoria": "categoria",
+                        "descricao": "descricao",
+                        "funcionarios": [],
+                        "variantes": []
+                    }
+                   """)).andExpect(status().isConflict());
+    }
+
+    @Test
+    void testeServicoControllerPostBadRequest() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.post("/servicos")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                    {
+                        "nome": "banho",
+                        "especies": [],
+                        "categoria": "categoria",
+                        "descricao": "descricao",
+                        "funcionarios": [],
+                        "variantes": []
+                    }
+                   """))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void testeServicoControllerPostInternalServerError() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.post("/servicos")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                    {
+                        "nome": "banho",
+                        "especies": [],
+                        "categoria": "categoria",
+                        "descricao": "descricao",
+                        "funcionarios": [],
+                        "variantes": []
+                    }
+                   """))
+                .andExpect(status().isInternalServerError());
+    }
+
 
 }
